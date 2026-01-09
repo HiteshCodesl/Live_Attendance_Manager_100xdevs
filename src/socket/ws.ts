daa153025ca3fb50e08f9845760f778d9378a577
Hitesh Navare <navarehitesh65@gmail.com>
2026-01-10 00:39:40 +0530

    added the attendance marked case in message

--- src/socket/ws.ts
+++ src/socket/ws.ts
@@ -2,6 +2,8 @@ import "express-ws";
 import type { Application } from "express-ws";
 import jwt, { type JwtPayload } from "jsonwebtoken"
 import type WebSocket from "ws";
+import { activeSession } from "../routes/classRoute.js";
+import { classModel, userModel } from "../db/db.js";
 
 interface WebSocketInterface {
     ws: WebSocket,
@@ -9,35 +11,70 @@ interface WebSocketInterface {
     role: string
 }
 
-const allWebSocketConnections: WebSocketInterface[] = [];
+let allWebSocketConnections: WebSocketInterface[] = [];
 
 export function webSocketServer(wsserver: Application) {
     wsserver.ws('/ws', (ws, req) => {
-
+        const socket = ws as WebSocket;
         const token = req.query.token as string;
 
         if (!token) {
             ws.close();
             return;
         }
-        
+
         const { id, role } = jwt.verify(token, process.env.JWT_SECRET!) as JwtPayload;
-            
+
+        socket.user = { userId: id, role }
+
         allWebSocketConnections.push({ ws, userId: id, role });
-   
-        ws.on('message', (data) => {
-            ws.send(data.toString());
-            console.log(id, role);
-            console.log(data.toString());
+
+        ws.on('message', async(data) => {
+            const parsedData = JSON.parse(data.toString());
+            switch (parsedData.type) {
+                case "ATTENDANCE_MARKED":
+                    if (socket.user?.role !== 'teacher' && !activeSession) {
+                        ws.send(JSON.stringify({
+                            "event": "ERROR",
+                            "data": {
+                                "message": "Unauthorized or invalid token"
+                            }
+                        }))
+                    } else {
+                        activeSession.attendance[parsedData.data.studentId] = parsedData.data.status;
+
+                        allWebSocketConnections.forEach((student) => {
+                            student.ws.send(JSON.stringify({
+                                "event": "ATTENDANCE_MARKED",
+                                "data": {
+                                    "studentId": parsedData.data.studentId,
+                                    "status": parsedData.data.status
+                                }
+
+                            }))
+                        })
+                    }
+                case "TODAY_SUMMARY":
+                    if (socket.user?.role !== 'teacher' || socket.user.userId != activeSession.teacherId) {
+                        ws.send(JSON.stringify({
+                            "event": "ERROR",
+                            "data": {
+                                "message": "Unauthorized or invalid token"
+                            }
+                        }))
+                    } else {
+                      
+
+                    }
+            }
         })
 
         ws.on('close', () => {
-            allWebSocketConnections.filter(x => x.ws !== ws)
+            allWebSocketConnections = allWebSocketConnections.filter(x => x.ws !== ws);
         })
-
-        console.log(allWebSocketConnections);
     })
 }
+
 
 
 
